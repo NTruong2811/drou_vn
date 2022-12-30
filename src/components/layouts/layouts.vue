454137d540d32ddc8336766a31f1000da7f50426
NTruong <nhattruong.dev3@gmail.com>
2022-12-30 11:47:45 +0700

    update

---
+++ src/components/layouts/layouts.vue
@@ -0,0 +1,12 @@
+<template>
+      <h1>head</h1>
+      <nav><router-link to="/">Home</router-link><router-link to="/shop">shop</router-link></nav>
+      <router-view></router-view>
+      <h1>footer</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
