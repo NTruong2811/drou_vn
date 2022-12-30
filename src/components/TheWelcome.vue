1ce978e243fe58da5da02048170644ca7a96cf3e
tavanhoi2000 <121653451+tavanhoi2000@users.noreply.github.com>
2022-12-30 17:06:05 +0700

    sửa

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -12,7 +12,7 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentation a</template>
+    <template #heading>Documentation hfgjkfg</template>
     Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
     provides you with all information you need to get started.
