fc83ebcc8543ccb888dd4a1a4d93c22f0a90d781
NTruong <nhattruong.dev3@gmail.com>
2022-12-30 17:19:58 +0700

    abc

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -12,7 +12,7 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentation 123truong</template>
+    <template #heading>Documentation abc</template>
 
     Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
