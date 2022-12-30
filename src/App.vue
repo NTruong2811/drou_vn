8ee74525d77dc7e1d8e501ca7df38f09213d5202
tavanhoi2000 <tavanhoi000@gmail.com>
2022-12-30 15:54:01 +0700

    ass

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -11,6 +10,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
+
+
+        <h3>Hello </h3>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
