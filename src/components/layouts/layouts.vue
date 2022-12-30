83736509ac753d589c193c4ee6ddb87960f7a170
NTruong <nhattruong.dev3@gmail.com>
2022-12-30 17:53:16 +0700

    update

---
+++ src/components/layouts/layouts.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>head</header>
+    <nav>
+        <ul>
+            <li><router-link to="/">home</router-link></li>
+            <li><router-link to="/shop">shop</router-link></li>
+        </ul>
+    </nav>
+      <router-view></router-view>
+      <footer>foot</footer>
+</template>
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import layouts from '../components/layouts/layouts.vue'
+import HomePage from '../components/pages/HomePage.vue'
+import ShopPage from '../components/pages/ShopPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: layouts,
+      children:[
+        {
+          path:'/',
+          name: 'home',
+          component:HomePage
+        },
+        {
+          path:'/shop',
+          name: 'shop',
+          component:ShopPage
+        },
+      ]
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
   ]
 })
 
