755d63777b16b44605bffc9245046cfa19f7fc6b
NTruong <nhattruong.dev3@gmail.com>
2023-01-05 23:09:20 +0700

    update

---
+++ src/components/components/BreadcrumbComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="breadcrumb">
+    <ul id="breadcrumb" class="container"></ul>
+  </div>
+</template>
+<style>
+.breadcrumb {
+  height: 125px;
+  background-color: #f4f4f4 !important;
+}
+.breadcrumb .container {
+  margin-top: 0px !important;
+  display: flex;
+  align-items: center;
+}
+</style>
+<script>
+export default {
+  setup() {
+    return {};
+  },
+  mounted() {
+    var breadcrumb = document.getElementById("breadcrumb");
+    var url = window.location.pathname;
+    var arr = url.split("/");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode("Home/"));
+    breadcrumb.appendChild(li);
+    for (let index = 0; index < arr.length; index++) {
+      if (arr[index] != "") {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(arr[index]));
+        breadcrumb.appendChild(li);
+      }
+    }
+  },
+};
+</script>
--- src/components/pages/ShopPage.vue
+++ src/components/pages/ShopPage.vue
@@ -1,3 +1,16 @@
 <template>
-    shop
-   </template>
+ <breadcrumb-component-vue></breadcrumb-component-vue>
+
+</template>
+
+<script>
+import BreadcrumbComponentVue from '../components/BreadcrumbComponent.vue'
+export default {
+    components:{
+        BreadcrumbComponentVue         
+    },
+    setup() {
+        
+    },
+}
+</script>
